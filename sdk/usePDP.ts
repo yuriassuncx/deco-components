d51a74d405ab5b53b38e39682869e67e8ba58a7b
@yuri_assuncx <killerkoll2012@gmail.com>
2024-05-13 18:31:42 -0300

    Update usePDP.ts

--- sdk/usePDP.ts
+++ sdk/usePDP.ts
@@ -2,9 +2,9 @@ import { Product } from "apps/commerce/types.ts";
 import { signal } from "@preact/signals";
 import { Sku } from "./useVariantPossibilitiesClientSide.ts";
 
-const productSelected = signal(<Product | null> null);
+const productSelected = signal<Product | null>(null);
 
-const skuSelected = signal(<Sku | null> null);
+const skuSelected = signal<Sku | null>(null);
 
 const state = {
   productSelected,
